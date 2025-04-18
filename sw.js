5fd75507e716ee2bf980aa03896270a1454fc4d3
kgv <kgv@users.noreply.github.com>
2025-04-18 17:53:59 +0000

    Deploying to gh-pages from @ ippras-blcs/_viewer@981d3ac42631c8416f46eb58a605c3b6827a4fcc 🚀

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'blc';
+var filesToCache = [
+  './',
+  './index.html',
+  './blc.js',
+  './blc_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
